d0a936eff2ebd7df08022d46052757dbaeacfb4f
manerao.pritam <manerao.pritam@gmail.com>
2025-01-31 16:04:25 +0000

    Pushing Task entity and it's crud operations

---
+++ src/main/java/org/example/TaskManager.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.example.TaskUtil.isValid;
+
+public class TaskManager {
+    private final List<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        if (!isValid(task)) {
+            System.out.println("Error adding task " + task.getTitle() + ". Task due date can't be in the past.");
+            return;
+        }
+        tasks.add(task);
+    }
+
+    public List<String> getAllTasks() {
+        return this.tasks.stream()
+                .map(Task::toString)
+                .toList();
+    }
+
+    public Task getTaskById(final UUID taskId) {
+        for (Task task: this.tasks) {
+            if (task.getId().equals(taskId)) {
+                return task;
+            }
+        }
+
+        System.out.println("Task with id " + taskId + " not found.");
+        return null;
+    }
+
+    public Task updateTask(final UUID taskId, final Task taskToUpdate) {
+        // validate task
+        if (!isValid(taskToUpdate)) {
+            System.out.println("Update failed. Task due date can't be in the past.");
+            return null;
+        }
+
+        Task savedTask = this.getTaskById(taskId);
+        if (savedTask == null) {
+            return null;
+        }
+
+        savedTask.setTitle(taskToUpdate.getTitle());
+        savedTask.setDueDate(taskToUpdate.getDueDate());
+        savedTask.setCompleted(taskToUpdate.isCompleted());
+
+        System.out.println("Task updated.");
+        return savedTask;
+    }
+
+    public void deleteTask(final UUID taskId) {
+        boolean deleted = this.tasks.removeIf(task -> task.getId().equals(taskId));
+
+        if (deleted) {
+            System.out.println("Task deleted.");
+        } else {
+            System.out.println("Task with id " + taskId + " not found.");
+        }
+    }
+}
