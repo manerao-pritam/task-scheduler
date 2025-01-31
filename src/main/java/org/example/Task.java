376d439a9e1f6c4bc3c232c75c10d7583a179ecc
manerao.pritam <manerao.pritam@gmail.com>
2025-01-31 15:58:59 +0000

    Pushing Task entity and it's crud operations

---
+++ src/main/java/org/example/Task.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Task {
+    private final UUID id = UUID.randomUUID();
+    private String title;
+    private LocalDateTime dueDate;
+    private boolean isCompleted = false;
+
+    public Task(final String title, final LocalDateTime dueDate) {
+        this.title = title;
+        this.dueDate = dueDate;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", dueDate=" + dueDate +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return isCompleted == task.isCompleted && Objects.equals(id, task.id)
+                && Objects.equals(title, task.title) && Objects.equals(dueDate, task.dueDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, dueDate, isCompleted);
+    }
+}
